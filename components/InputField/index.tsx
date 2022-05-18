075c51427d91ba6d623bf080407c09ad572363c0
Labi Lawal <labihakin@gmail.com>
2022-05-18 10:12:12 +0100

    CLIENT: Signup form fullname, email, password validation complete

--- components/InputField/index.tsx
+++ components/InputField/index.tsx
@@ -1,14 +1,23 @@
 import styles from "./inputfield.module.css";
 
 export default function InputField(props:any) {
-    
-    const { type, label, value, hint, error, onKeyPress } = props;
+
+    const { type, label, hint, error, onKeyPress } = props;
 
     return(
         <div className={styles.input_field}>
-            <label className={styles.label}> {label} </label>
-            <input type={type} className={styles.input} onKeyDown={()=> onKeyPress()} />
-            <div className={styles.error}> {error} </div>
+            <label className={styles.label}> { label } </label>
+            <input type={type} className={styles.input} placeholder={hint} onInput={ (e:any)=> onKeyPress(e.target.value) } />
+            <div className={styles.error}> { error } </div>
         </div>
     );
 };
+
+
+
+
+
+
+
+
+// COLD BREW IS SWEETER, IT ALSO CONTAINS MORE CAFFIENE 
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,73 +1,122 @@
-import Link from "next/link";
 import { useState } from "react";
+import Link from "next/link";
 import TextButton from "../../components/Buttons/TextButton";
 import InputField from "../../components/InputField";
 import styles from "../../styles/auth.module.css";
 
-const [fullnameModel, setFullnameModel] = useState({
-    type:'text',
-    value:'',
-    error:'',
-    label:'Full Name',
-    hint: 'eg. firstname lastname'
-}), 
-[emailModel, setEmailModel] = useState({
-    type: 'email',
-    value:'',
-    error:'',
-    label:'Email',
-    hint: ''
-}),
-[passwordModel, setPasswordModel] = useState({
-    type: 'password',
-    value:'',
-    error:'',
-    label:'Password',
-    hint: ''
-});
-
-
-const validateFullname = ()=> {
-    if(!fullnameModel.value) {
-        fullnameModel.error = 'Field cannot be empty'
-        return false
-    }
-    if(fullnameModel.value.split(' ').length != 2) {
-        fullnameModel.error = 'Please only provide your first and lastname seperated by a whitespace'
-        console.log(fullnameModel);
-        return false
-    }
-
-    fullnameModel.error = '';
-    return true;
-}
-
-const verifyEmail = ()=> {
-
-}
-
-// const verify
-
-const registerUser = ()=> {
-
-}
-
-
 export default function SignUp (props:any) {
+
+    const [fullnameModel, setFNModel] = useState({ //Fullname is shortened to FN for easy ref
+        type:'text',
+        name: 'fullname',
+        value:'',
+        error:'', 
+        label:'Full Name',
+        hint: 'eg. firstname lastname'
+    }), 
+    [emailModel, setEmailModel] = useState({
+        type: 'email',
+        name: 'email',
+        value:'',
+        error:'',
+        label:'Email',
+        hint: ''
+    }),
+    [passwordModel, setPasswordModel] = useState({
+        type: 'password',
+        name: 'password',
+        value:'',
+        error:'',
+        label:'Password',
+        hint: ''
+    });
+    
+    const setInput = (inputItem:any, model:any)=> {
+        const updated = {...model};
+        updated.value = inputItem;
+        if(model.name == 'fullname') {
+            validateFN(updated);
+            setFNModel(updated);
+        }
+        if(model.name == 'email') { 
+            validateEmail(updated)
+            setEmailModel(updated);
+        }
+        if(model.name == 'password') { 
+            validatePassword(updated)
+            setPasswordModel(updated);
+        }
+    }
+
+    const validateFN = (updateItem:any)=> {
+        if(!updateItem.value) {
+            updateItem.error = 'Field cannot be empty'
+            return false;
+        }
+        if(updateItem.value.split(' ').length < 2) {
+            updateItem.error = 'Please both your first and last names are needed'
+            return false;
+        }
+        if(updateItem.value.split(' ').length > 2) {
+            updateItem.error = 'Please only your first and last names are needed'
+            return false;
+        }
+        if(updateItem.value.split(' ')[1] == '' || updateItem.value.split(' ')[0] == '') {
+            updateItem.error = 'Please both your first and last names are needed'
+            return false;
+        }
+
+        updateItem.error = ''
+        return true;
+    }
+
+    const validateEmail = (updateItem:any)=> {
+        const emailRegExp = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+        if(!updateItem.value) {
+            updateItem.error = 'Field cannot be empty'
+            return false;
+        }
+        if(!emailRegExp.test(updateItem.value)) {
+            updateItem.error = 'Email is not valid';
+            return false;
+        }
+
+        updateItem.error = ''
+        return true;
+    }
+
+    const validatePassword = (updateItem:any)=> {
+        if(!updateItem.value) {
+            updateItem.error = 'Field cannot be empty'
+            return false;
+        }
+        if(updateItem.value.length < 8) {
+            updateItem.error = 'Must be minimum of 8 characters'
+            return false;
+        }
+
+        updateItem.error = ''
+        return true;
+    }
+
+    function preventFormDefault(e:any) { return e.preventDefault; }
+
     return (
         <section>
             <div className={styles.title}>BUGGER</div>
 
             <form className={styles.form_frame}>
                 <div className={styles.heading}>Create a new account</div>
+
                 <div className={styles.input_wrapper}>
-                    <InputField 
+                    <InputField
                         type={fullnameModel.type}
                         label={fullnameModel.label}
-                        value={fullnameModel.value}
+                        value={fullnameModel.value} 
                         hint={fullnameModel.hint}
                         error={fullnameModel.error}
-                        onKeyPress={validateFullname}
+                        onKeyPress={(input:any)=> setInput(input, fullnameModel)}
                     />
                 </div>
                 <div className={styles.input_wrapper}>
@@ -77,7 +126,7 @@ export default function SignUp (props:any) {
                         value={emailModel.value}
                         hint={emailModel.hint}
                         error={emailModel.error}
-                        onKeyPress={validateFullname}
+                        onKeyPress={(input:any)=> setInput(input, emailModel)}
                     />
                 </div>
                 <div className={styles.input_wrapper}>
@@ -87,12 +136,12 @@ export default function SignUp (props:any) {
                         value={passwordModel.value}
                         hint={passwordModel.hint}
                         error={passwordModel.error}
-                        onKeyPress={validateFullname}
+                        onKeyPress={(input:any)=> setInput(input, passwordModel)}
                     />
                 </div>
-                
+                 
                 <div className={styles.signin_btn_wrapper}>
-                    <TextButton label="SIGN UP" onclick={registerUser} />
+                    <TextButton label="SIGN UP" />
                 </div>
 
                 <div className={styles.gotosignup}>
